719312f88a9cb596bf5866d27193bd7332856601
Khoa Pham <kenjifam@gmail.com>
2018-10-31 22:33:08 +0200

    config store

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
 import React, { Component } from 'react';
-import './App.css';
+import { Provider } from 'react-redux';
+import { configureStore } from './store';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      i: 0,
-      list: ['Test 1', 'Test 2', 'Test 3', 'Hihi ^^']
-    };
-  }
+const store = configureStore();
 
-  componentDidMount() {
-    setInterval(() => {
-      let { i, list } = this.state;
-      i = i >= list.length - 1 ? 0 : i + 1;
-      this.setState({ i: i });
-    }, 1000);
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <h1>{this.state.list[this.state.i]}</h1>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <div>Hello</div>
+    </Router>
+  </Provider>
+);
 
 export default App;
